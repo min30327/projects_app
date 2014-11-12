7dd345fbb268af3fe21698b8f078a80cff921a9b
Mineo Okuda <min30327@gmail.com>
2014-11-12 21:57:05 +0900

    （；＾ν＾）ぐぬぬ…

--- project_app/Controller/ProjectsController.php
+++ project_app/Controller/ProjectsController.php
@@ -110,7 +110,7 @@ class ProjectsController extends AppController {
 		$this->paginate = array(
 	                'Message' => array(
 	                    'conditions' => array('project_id'=>$id),
-	                    'order' => array('modified'=>'desc'),
+	                    'order' => array('Message.modified desc'),
 	                    'limit' =>10,
 	                    'paramType' => 'querystring'
 	                )
